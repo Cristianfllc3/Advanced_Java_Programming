dfc049aa9043ecfbc9d9fd0a6592f014d504d9cb
Cristian <cristian.c3.fl@gmail.com>
2021-02-09 16:10:12 -0500

    Add generic example

---
+++ src/Generics/GenericMethods.java
@@ -0,0 +1,10 @@
+package Generics;
+
+public class GenericMethods {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+}
---
+++ src/Generics/GenericsExample.java
@@ -0,0 +1,27 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsExample {
+
+    public static void main(String[] args) {
+
+        // Example without Generics
+        List names = new ArrayList();
+        names.add("Cristian");
+        String name = (String) names.get(0);
+        System.out.println("First name " + name);
+        //names.add(7);
+
+        // Example with Generics
+        List<String> namesG = new ArrayList<>();
+        namesG.add("Cristian");
+        String nameG = namesG.get(0);
+        System.out.println("First name " +nameG);
+        //namesG.add(7);
+        //This throws an error because the list is cast to <String>,
+        //Without generic it is necessary cast each element of the list "(String) names.get(0);"
+    }
+
+}
