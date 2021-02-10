195e42ac73c2849ea13a601d527f9ee0ccc56dab
Cristian <cristian.c3.fl@gmail.com>
2021-02-10 15:39:12 -0500

    Add files with example of functional programing

---
+++ src/Functional_programing/functionalinterfaceexample/Main.java
@@ -0,0 +1,18 @@
+
+package Functional_programing.functionalinterfaceexample;
+
+/**
+ *
+ * @author bethan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        
+    }
+
+    
+}
