9b44eec1841c98a1a06be661d03bb5c3513cde3c
Cristian <cristian.c3.fl@gmail.com>
2021-02-10 16:40:03 -0500

    First Lambdas example !

---
+++ src/Functional_programing/lambdaexample/MessagePrinter.java
@@ -0,0 +1,13 @@
+
+package Functional_programing.lambdaexample;
+
+/**
+ *
+ * @author bethan
+ */
+@FunctionalInterface
+public interface MessagePrinter {
+    
+    public abstract void printMessage();
+    
+}
