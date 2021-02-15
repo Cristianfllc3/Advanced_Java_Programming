63b25eda74c08d4fd3608788a5e1a3f9ebc49a3b
Cristian <cristian.c3.fl@gmail.com>
2021-02-14 21:38:24 -0500

    .

--- src/Multithreading/runnable_interface/Main.java
+++ src/Multithreading/runnable_interface/Main.java
@@ -9,6 +9,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //A second way ot running a thread in java it is with a interface
+        Thread thread1 = new Thread(new RunnableExample());
+        thread1.start();
+        Thread thread2 = new Thread(new RunnableExample());
+        thread2.start();
+        Thread thread3 = new Thread(new RunnableExample());
+        thread3.start();
+
+
     }
 
 }
